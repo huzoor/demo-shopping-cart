9bfaa3997b2595b148771c11e7a9281e4d7c56d1
huzoor <huzoorittec@gmail.com>
2022-03-12 09:05:18 +0530

    Adding body content

--- src/components/confirmrx.js
+++ src/components/confirmrx.js
@@ -5,10 +5,11 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import {Sanitizer, CancelOutlined} from '@mui/icons-material';
 
 import Header from "../shared/header";
 import Footer from "../shared/footer";
-import { padding } from "@mui/system";
+
 const useStyles = makeStyles( theme => ({
     root: {
        
@@ -17,9 +18,22 @@ const useStyles = makeStyles( theme => ({
     header:{
         backgroundColor: theme.palette.primary.main,
         padding: '10px',
+        position: 'fixed',
+        width: '95%'
+    },
+    bodyContent:{
+        height: '94vh',
+        paddingTop: '70px',
+    },
+    rxHeader:{
         display: 'flex',
-       justifyContent: 'center'
-    }
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    footer:{
+        backgroundColor: theme.palette.primary.main,
+    },
+
   }));
 const ConfirmRx = (props) =>{
     const classes = useStyles();
@@ -29,15 +43,39 @@ const ConfirmRx = (props) =>{
             <Container maxWidth="xlg" >
               <Box sx={{  bgcolor: '#fff', height: '100vh', paddingTop: '10px'}} >
                 <Box sx={{ flexGrow: 1 }}>
-                    <Grid container  rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 3, xlg:4 }}>
+                    <Grid  rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 3, xlg:4 }}>
                         <Grid item xs={12} className={classes.header}>
                             <Header />
                         </Grid>
-                        <Grid item xs={12}>
-                            <div>
-                                Body Contnt</div>
+                        <Grid  item xs={12}  className={classes.bodyContent} >
+                            <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                                <Grid item xs={12}>
+                                    <Paper variant="outlined" >
+                                        <div className={classes.rxHeader}>
+                                            <div> <Sanitizer/> </div>
+                                            <div>RX:  8560099-59106</div>
+                                            <div><CancelOutlined/></div>
+                                        </div>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Paper variant="outlined" >
+                                            row 2
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Paper variant="outlined" >
+                                            row 3
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Paper variant="outlined" >
+                                            row 4
+                                    </Paper>
+                                </Grid>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} className={classes.header}>
+                        <Grid item xs={12} className={classes.footer}>
                             <Footer />
                         </Grid>
                         
--- src/shared/header.js
+++ src/shared/header.js
@@ -1,7 +1,23 @@
 import React, {useState} from "react";
+import { makeStyles  } from '@mui/styles';
 
-const Header = ()=>{
-    return <div> Header </div>
+const useStyles = makeStyles( theme => ({
+    
+    headerMain:{
+        display: 'flex',
+        justifyContent:'center',
+        color: '#fff'
+    },
+    headerLogo:{
+        height: '25px',
+        width: '25px'
+    } 
+
+}));
+
+const Header = () => {
+    const classes = useStyles();
+    return <div className={classes.headerMain}> <span><img className={classes.headerLogo} src="https://www.walgreens.com/images/adaptive/mobileapps/Pharmacy/Walgreens_Corner_Logo.png" /></span>Confirm Rx </div>
 }
 
 export default Header;
